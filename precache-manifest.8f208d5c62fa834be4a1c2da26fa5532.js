f788b09f98379b6cf94dfe36e84a0ff7f24ce662
CoreyDennison <cmgdennison@gmail.com>
2020-08-18 15:33:54 +0100

    Updates

---
+++ precache-manifest.8f208d5c62fa834be4a1c2da26fa5532.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf3784944954a9496d1df8e3da7a861c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5f763ec41696c47b530",
+    "url": "/static/css/main.de53bf85.chunk.css"
+  },
+  {
+    "revision": "b067300af65e014b3b77",
+    "url": "/static/js/2.6c465242.chunk.js"
+  },
+  {
+    "revision": "14e4254cfb167efa11ed5ed0deb48cdc",
+    "url": "/static/js/2.6c465242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f763ec41696c47b530",
+    "url": "/static/js/main.dbe6b60a.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  },
+  {
+    "revision": "1d2c7d34a626fe9204df39aba90aadc5",
+    "url": "/static/media/artist.1d2c7d34.jpg"
+  },
+  {
+    "revision": "dcfd34c6649a545d2a4897e9a7b95831",
+    "url": "/static/media/display.dcfd34c6.jpg"
+  },
+  {
+    "revision": "0ebb4655fb7845f77b0e123b23bf25dd",
+    "url": "/static/media/joc.0ebb4655.jpg"
+  },
+  {
+    "revision": "b542486ffd60cca7cd6ba3dbfd2137c3",
+    "url": "/static/media/mm.b542486f.jpg"
+  },
+  {
+    "revision": "b2bd6b641492cd996cee9ba4645c1a80",
+    "url": "/static/media/otb.b2bd6b64.jpg"
+  }
+]);
