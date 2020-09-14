29f7ea577b4737ae771541f248681aee22073f8c
Corey Dennison <cmgdennison@gmail.com>
2020-09-14 11:26:47 +0100

    Created functional build of React Portfolio
    Corrected paths in index.html file and undertook cross-browser compatibility. Fully functional build. Add Google Analytics and events for React.

---
+++ build/precache-manifest.c56f2b1302a5814f6ca076c1188e1012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb4c3bccdd70415299e9d45f29f0e9d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "800f1b10ce4cd613f659",
+    "url": "/static/css/main.77f811b9.chunk.css"
+  },
+  {
+    "revision": "298aafcf30b2600a2e42",
+    "url": "/static/js/2.ebb39ca4.chunk.js"
+  },
+  {
+    "revision": "14e4254cfb167efa11ed5ed0deb48cdc",
+    "url": "/static/js/2.ebb39ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "800f1b10ce4cd613f659",
+    "url": "/static/js/main.3aaa6b7f.chunk.js"
+  },
+  {
+    "revision": "e772e3ee0db6f9829b44",
+    "url": "/static/js/runtime-main.fdaf22cd.js"
+  }
+]);
